337e4ee72213b0b49a44a4b4d8b9c3ca9346eff2
jhenniifer <chukwumakosisochukwu7gmail.com.gmail.com>
2025-01-04 20:45:26 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamBurger = document.querySelector('.toggle__btn');
+const navLinks = document.querySelector('.nav__link__mobile__container');
+const icon = document.querySelector('.fa-solid')
+
+hamBurger.addEventListener('click',function(){
+    navLinks.classList.toggle('open');
+    if(icon.classList.contains('fa-bars')){
+        icon.classList.replace('fa-bars', 'fa-x');
+    } else{
+        icon.classList.replace('fa-x', 'fa-bars');
+    }
+    
+})
